9f5cad6b7872fc9aa0f2bb139a2c2a803eebb97d
marco pegoraro <marco.pegoraro@gmail.com>
2020-08-20 10:09:45 +0200

    Add vars to skip part of the migrations

--- services/migrations/docker-migrate.sh
+++ services/migrations/docker-migrate.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env sh
 
-hasura migrate apply --endpoint ${HASURA_ENDPOINT} --admin-secret ${HASURA_ADMIN_SECRET} --skip-update-check
-hasura metadata apply --endpoint ${HASURA_ENDPOINT} --admin-secret ${HASURA_ADMIN_SECRET} --skip-update-check
-hasura seeds apply --endpoint ${HASURA_ENDPOINT} --admin-secret ${HASURA_ADMIN_SECRET} --skip-update-check
+if [ -z "${HASURA_SKIP_MIGRATIONS}" ]
+then
+    hasura migrate apply --endpoint ${HASURA_ENDPOINT} --admin-secret ${HASURA_ADMIN_SECRET} --skip-update-check
+fi
+
+if [ -z "${HASURA_SKIP_METADATA}" ]
+then
+    hasura metadata apply --endpoint ${HASURA_ENDPOINT} --admin-secret ${HASURA_ADMIN_SECRET} --skip-update-check
+fi
+
+if [ -z "${HASURA_SKIP_SEEDS}" ]
+then
+    hasura seeds apply --endpoint ${HASURA_ENDPOINT} --admin-secret ${HASURA_ADMIN_SECRET} --skip-update-check
+fi
+
 
 # Do not let the process to end
 if [ -z "${HASURA_KEEPALIVE}" ]
