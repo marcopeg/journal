53d57ef1994420819d00cb57b5776cc0a014f05f
marco pegoraro <marco.pegoraro@gmail.com>
2020-08-20 10:00:04 +0200

    Keep process alive env var

--- services/migrations/docker-migrate.sh
+++ services/migrations/docker-migrate.sh
@@ -3,3 +3,6 @@
 hasura migrate apply --endpoint ${HASURA_ENDPOINT} --admin-secret ${HASURA_ADMIN_SECRET} --skip-update-check
 hasura metadata apply --endpoint ${HASURA_ENDPOINT} --admin-secret ${HASURA_ADMIN_SECRET} --skip-update-check
 hasura seeds apply --endpoint ${HASURA_ENDPOINT} --admin-secret ${HASURA_ADMIN_SECRET} --skip-update-check
+
+# Do not let the process to end
+[ ! -z "$HASURA_KEEPALIVE" ] || while true; do foo; sleep 20; done
